46898c723716fbac7541f4a258c55403fc3adcfb
Harshit shukla <harshitshuklaharsh8@gmail.com>
2024-11-26 18:02:15 +0530

    2

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectToMongo = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const inventoryRoutes = require('./routes/inventoryRoute');
-const bookingRoutes = require('./routes/bookingRoute');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectToMongo = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const inventoryRoutes = require("./routes/inventoryRoute");
+const bookingRoutes = require("./routes/bookingRoute");
 const cors = require("cors");
-
 
 const app = express();
 
@@ -17,12 +16,10 @@ connectToMongo();
 //middlewares
 app.use(express.json()); //parse json bodies
 
-
 //routes
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/inventory', inventoryRoutes);
-app.use('/api/v1/booking', bookingRoutes);
-
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/inventory", inventoryRoutes);
+app.use("/api/v1/booking", bookingRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
