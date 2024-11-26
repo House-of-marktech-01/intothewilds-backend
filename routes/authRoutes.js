ef9803438c372d38af78a9fab456959d493956a6
Harshit shukla <harshitshuklaharsh8@gmail.com>
2024-11-26 15:01:36 +0530

    1

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,21 @@
 // server/routes/authRoutes.js
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authController = require('../controller/authController')
-const { authenticateToken, authorizeRole } = require('../middleware/authMiddleware');
-
+const authController = require("../controller/authController");
+const {
+  authenticateToken,
+  authorizeRole,
+} = require("../middleware/authMiddleware");
 
 // Register Route
-router.post('/register', authenticateToken, authorizeRole('admin'), authController.register);
+router.post(
+  "/register",
+  authenticateToken,
+  authorizeRole("admin"),
+  authController.register
+);
 
 // Login Route
-router.post('/login', authController.login);
+router.post("/login", authController.login);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,32 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const connectToMongo = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const inventoryRoutes = require('./routes/inventoryRoute');
-const bookingRoutes = require('./routes/bookingRoute');
+const express = require("express");
+const dotenv = require("dotenv");
+const connectToMongo = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+const inventoryRoutes = require("./routes/inventoryRoute");
+const bookingRoutes = require("./routes/bookingRoute");
+
+const cors = require("cors"); // Add this line
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Your frontend's exact origin
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
 connectToMongo();
 
 //middlewares
 app.use(express.json()); //parse json bodies
 
-
 //routes
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/inventory', inventoryRoutes);
-app.use('/api/v1/booking', bookingRoutes);
-
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/inventory", inventoryRoutes);
+app.use("/api/v1/booking", bookingRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
